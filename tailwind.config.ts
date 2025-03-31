2e142869fc6b6f2e8a7c2e9995158510d117a080
me <66961093+EvilCultist@users.noreply.github.com>
2025-03-31 20:52:13 +0530

    Programming the flux capacitor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,3 +12,25 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+
+// colors
+//
+// #fdf0d5 cream white
+// #14213d dark blue
+// #669bbc light blue
+// #fb8500 orange dark
+//
+//
+// 03045e blue
+// bbd0ff
+// b8c0ff
+// c8b6ff
+// e7c6ff
+// ffd6ff
+//
+// 9d8189
+// f4acb7
+// ffcad4
+// ffe5d9
+// d8e2dc
